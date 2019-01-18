70689c844a49138e9f2432404c3a2e382fc69c24
Bruno Ricardo Secco <brunodvic3@hotmail.com>
2019-01-18 10:12:29 -0200

    Using props and steteless components

---
+++ myapp/src/Brunos.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+//em um stateless component as props precisam ser especificadas, como não é uma classe e sim uma funcção, não tem como usar o metodo this, sendo assim não tem como referenciar props. Então explicitamente declarase a prop, por meio de "(props)" e em seguida "const bruno = props", ou de um modo mais direto usasse a desestruturação "({brunos}, {exelplo2}, {exemplo3}). De ambas as formas daria certo"
+const Brunos = ({brunos}/* comentario acima se refere a isso */) => {
+    const brunoList = brunos.map(bruno => {
+      return (
+        <div className="bruno" key={bruno.id}>
+          <p>Name: {bruno.name}</p>
+          <p>Age: {bruno.age}</p>
+          <p>Teeth: {bruno.teeth}</p>
+        </div>
+      );
+    })
+    return(
+      <div className="bruno-list">
+        { brunoList }
+      </div>
+    );
+}
+
+
+export default Brunos;
