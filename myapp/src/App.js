afcbdd2e30f929ae9e1437e67f5420b8234ade96
Bruno Ricardo Secco <brunodvic3@hotmail.com>
2019-01-18 09:55:30 -0200

    Learn to use a outpitting component list

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
+import Brunos from "./Brunos";
 
 class App extends Component {
+  state = {
+    brunos: [
+      { name: "bruno", age: 21, teeth: "white", id: 0 },
+      { name: "joao", age: 24, teeth: "brown", id: 1 },
+      { name: "filip", age: 30, teeth: "blsck", id: 2 },
+    ],
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="app">
+        <h1>My first application</h1>
+        <p>oi</p>
+        <Brunos brunos={this.state.brunos}/> 
+      </div> //props são atributos passados do componente pai para um componente filho, um exemplo são name, age e teeth. Eles são usados como variaveis no componente filho
     );
   }
 }
