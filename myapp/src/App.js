5301be535f8efbb5bea3aa9fc9c5516e560d3093
Bruno Ricardo Secco <brunodvic3@hotmail.com>
2019-03-25 18:51:13 -0300

    get all the basics informations

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Brunos from "./Brunos";
 import AddBruno from "./AddBruno";
 
 class App extends Component {
   state = {
     brunos: [
-      { name: "bruno", age: 21, teeth: "white", id: 0 },
-      { name: "joao", age: 24, teeth: "brown", id: 1 },
-      { name: "filip", age: 30, teeth: "blsck", id: 2 },
-    ],
-  }
+    ]
+  };
 
-  addBruno = (bruno) => { //o argumento bruno é um estado que é passado pelo componente filho.
+  addBruno = bruno => {
+    //o argumento bruno é um estado que é passado pelo componente filho.
     bruno.id = Math.random(); // é gerado um id automaticamente *de forma horrivel*
     let brunos = [...this.state.brunos, bruno]; //a form mais facil de atualizar um state de array é refazendo um array e adicionando um novo registro ao array. A propriedade rest "..." adiciona cada registro em um objeto próprio, e ao final é adicionado um novo registro
     this.setState({
       brunos: brunos //o estado é atualizado.
-    })
-  }
+    });
+  };
+
+  deleteBruno = id => {
+    let brunos = this.state.brunos.filter(bruno => {
+      return bruno.id !== id;
+    });
+    this.setState({ brunos: brunos });
+    console.log(this.state.brunos);
+  };
+
   render() {
     return (
       <div className="app">
         <h1>My first application</h1>
         <p>oi</p>
-        <Brunos brunos={this.state.brunos}/> 
-        <AddBruno addBruno={this.addBruno}/>
+        <Brunos deleteBruno={this.deleteBruno} brunos={this.state.brunos} />
+        <AddBruno addBruno={this.addBruno} />
         {/* uma função é passada como prop para poder ser recebido um dado */}
       </div> //props são atributos passados do componente pai para um componente filho, um exemplo são name, age e teeth. Eles são usados como variaveis no componente filho
     );
--- myapp/src/Brunos.js
+++ myapp/src/Brunos.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Brunos.css";
 
 //em um stateless component as props precisam ser especificadas, como não é uma classe e sim uma funcção, não tem como usar o metodo this, sendo assim não tem como referenciar props. Então explicitamente declarase a prop, por meio de "(props)" e em seguida "const bruno = props", ou de um modo mais direto usasse a desestruturação "({brunos}, {exelplo2}, {exemplo3}). De ambas as formas daria certo"
-const Brunos = ({brunos}/* comentario acima se refere a isso */) => {
+const Brunos = ({brunos, deleteBruno}/* comentario acima se refere a isso */) => {
 /*     const brunoList = brunos.map(bruno => {
       if(bruno.age > 21) {
         return (
@@ -16,12 +17,13 @@ const Brunos = ({brunos}/* comentario acima se refere a isso */) => {
       }
     }) */
     const brunoList = brunos.map(bruno => {
-      if(bruno.age > 21) {
+      if(bruno.age > 5) {
         return (
           <div className="bruno" key={bruno.id}>
             <p>Name: {bruno.name}</p>
             <p>Age: {bruno.age}</p>
             <p>Teeth: {bruno.teeth}</p>
+            <button onClick={() => {deleteBruno(bruno.id)}}>Delete person</button>
           </div>
         );
       } else {
